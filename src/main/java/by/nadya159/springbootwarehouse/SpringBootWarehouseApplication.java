3059dc634ede8a263215377692ce8697f494c301
Nadya159 <nadezdovna59@gmail.com>
2024-03-06 16:14:20 +0500

    Init

---
+++ src/main/java/by/nadya159/springbootwarehouse/SpringBootWarehouseApplication.java
@@ -0,0 +1,13 @@
+package by.nadya159.springbootwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/nadya159/springbootwarehouse/SpringBootWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package by.nadya159.springbootwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
